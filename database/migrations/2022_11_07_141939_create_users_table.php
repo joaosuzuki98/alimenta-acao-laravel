40f1f926c1193d51309b535508240c9fdbef0df9
João Suzuki <joaosuzuki98@outlook.com>
2022-11-08 11:48:09 -0300

    Criada as migrações e o index

--- database/migrations/2022_11_07_141939_create_users_table.php
+++ database/migrations/2022_11_07_141939_create_users_table.php
@@ -15,8 +15,25 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->char('users');
+            $table->char('users', 120);
+            $table->char('password', 24);
+            $table->char('cnpj', 14);
+            $table->char('cpf', 11);
+            $table->char('endereco', 50);
+            $table->char('endereco_num', 4);
+            $table->char('bairro', 50);
+            $table->char('cidade', 50);
+            $table->char('estado', 2);
+            $table->char('cep', 8);
+            $table->char('complemento', 50);
+            $table->char('whatsapp', );
             $table->timestamps();
+        });
+
+        Schema::create('doacoes_info', function (Blueprint $table) {
+            $table->char('descricao');
+            $table->char('num_donatarios');
+            $table->char('num_donatarios_instituicoes');
         });
     }
 
---
+++ resources/views/index.blade.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+  <title>Alimenta ação | Doe alimentos</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+  <header>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+          <div class="container">
+            <div style="height: 70px; width: 70px; background-color: red;"></div>
+            <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavId">
+                <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Por que doar?</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Doe</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Receba</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sobre</a>
+                    </li>
+                </ul>
+                <button class="p-2 ms-3">
+                    Começar
+                </button>
+            </div>
+      </div>
+    </nav>
+  </header>
+
+  <main>
+    
+  </main>
+
+  <footer>
+
+    <!-- place footer here -->
+  </footer>
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
