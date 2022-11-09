c10d0936551a94a36533971355f964ebbcd33932
João Suzuki <joaosuzuki98@outlook.com>
2022-11-09 12:01:08 -0300

    Feita novas migrações

--- database/migrations/2022_11_07_141939_create_users_table.php
+++ database/migrations/2022_11_07_141939_create_users_table.php
@@ -15,25 +15,21 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->char('users', 120);
-            $table->char('password', 24);
-            $table->char('cnpj', 14);
-            $table->char('cpf', 11);
-            $table->char('endereco', 50);
-            $table->char('endereco_num', 4);
-            $table->char('bairro', 50);
-            $table->char('cidade', 50);
-            $table->char('estado', 2);
-            $table->char('cep', 8);
-            $table->char('complemento', 50);
-            $table->char('whatsapp', );
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->char('cnpj', 14)->nullable();
+            $table->char('cpf', 11)->nullable();
+            $table->string('endereco');
+            $table->string('bairro');
+            $table->string('cidade');
+            $table->string('estado');
+            $table->string('cep');
+            $table->string('complemento' );
+            $table->string('whatsapp');
             $table->timestamps();
-        });
-
-        Schema::create('doacoes_info', function (Blueprint $table) {
-            $table->char('descricao');
-            $table->char('num_donatarios');
-            $table->char('num_donatarios_instituicoes');
         });
     }
 
---
+++ database/migrations/2022_11_09_123817_create_anuncios_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('anuncios', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('descricao');
+            $table->string('num_donatarios');
+            $table->string('num_donatarios_instituicoes');
+            $table->boolean('concluido');
+            $table->string('foto')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('anuncions');
+    }
+};
